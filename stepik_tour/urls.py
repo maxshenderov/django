e375a65e83b43bac60b8a7c65ba7878a97a0f031
maxshenderov <maxshenderov@yandex.ru>
2020-06-21 21:09:55 +0300

    Initial commit

--- stepik_tour/urls.py
+++ stepik_tour/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 from tours.views import MainView, DepartureView, TourView
 
 urlpatterns = [
--- tours/views.py
+++ tours/views.py
@@ -1,21 +1,61 @@
 from django.shortcuts import render
 from django.http import Http404
 
+import random
+
+from tours.data import data
+
+
 # Create your views here.
 
 
 def MainView(request):
-    return render(request, 'index.html')
+    context = {"rangeMax": random.sample(range(1, 16), 6)}
+    return render(request, 'index.html', context=context)
 
 
 def DepartureView(request, departure):
     departures = {"msk": "Из Москвы", "spb": "Из Петербурга", "nsk": "Из Новосибирска", "ekb": "Из Екатеринбурга",
                   "kazan": "Из Казани"}
-
+    departuresFilter = []
+    pricemin = 10000000000
+    pricemax = 0
+    nightsmin = 10000000000
+    nightsmax = 0
+    tourcount = 0
+    for key, value in data(request)["tours"].items():
+        if value["departure"] == departure:
+            departuresFilter.append(key)
+            pricemax = max(value["price"], pricemax)
+            pricemin = min(value["price"], pricemin)
+            nightsmax = max(value["nights"], nightsmax)
+            nightsmin = min(value["nights"], nightsmin)
+            tourcount = tourcount + 1
     if not departures.get(departure):
         raise Http404
-    return render(request, 'departure.html')
+
+    departurename = departures[departure]
+    context = {"rangeMax": departuresFilter,
+               "departure": departure,
+               "departurename": departurename[0].lower() + departurename[1:len(departurename)],
+               "pricemax": pricemax,
+               "pricemin": pricemin,
+               "nightsmax": nightsmax,
+               "nightsmin": nightsmin,
+               "tourcount": tourcount,
+               }
+
+    return render(request, 'departure.html', context)
 
 
 def TourView(request, id):
-    return render(request, 'index.html')
+    tour = data(request)["tours"][id]
+    departure = data(request)["departures"][tour["departure"]]
+    stars = ""
+    i = 0
+    while i < int(tour["stars"]):
+        i = i + 1
+        stars = stars + "★"
+    context = {"tour": tour, "departure": departure[0].lower() + departure[1:len(departure)], id: id, "stars": stars}
+    print(stars)
+    return render(request, 'tour.html', context=context)
